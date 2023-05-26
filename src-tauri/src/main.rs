7c660890072bd0b63f74cb00be997c7e15e9a339
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-05-26 13:02:38 +0800

    Fix indentation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,25 +4,24 @@
     windows_subsystem = "windows"
 )]
 
-use rodio::OutputStream;
 use rodio::source::Source;
-// use midir::MidiInput;
+use rodio::OutputStream;
 use midir::{MidiInput, MidiInputConnection};
+
 // Import synth module
-mod synth;
 mod oscillator;
+mod synth;
 
-use synth::Synth;
-use synth::Envelope;
 use oscillator::Oscillator;
+use synth::{Synth, Envelope};
 
+use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 use tauri::{Manager, Window, Wry};
-use serde::{Serialize, Deserialize};
 
 #[derive(Default)]
 struct MidiState {
-  pub input: Mutex<Option<MidiInputConnection<()>>>,
+    pub input: Mutex<Option<MidiInputConnection<()>>>,
 }
 
 struct SynthState {
@@ -31,65 +30,60 @@ struct SynthState {
 
 #[derive(Clone, Serialize, Deserialize)]
 struct MidiMessage {
-  message: Vec<u8>,
+    message: Vec<u8>,
 }
 
 #[tauri::command]
-fn open_midi_connection(
-  midi_state: tauri::State<'_, MidiState>,
-  window: Window<Wry>,
-) {
-  let handle = Arc::new(window).clone();
-  let midi_in = MidiInput::new("ui_synth");
-  match midi_in {
-    Ok(midi_in) => {
-      let midi_in_ports = midi_in.ports();
-      let port = midi_in_ports.get(0);
-      match port {
-        Some(port) => {
-            // Print the name of the port
-            println!("Port: {}", midi_in.port_name(port).unwrap());
-          let midi_in_conn = midi_in.connect(
-            port,
-            "midir",
-            move |_, message, _| {
-                println!("Message: {:?}", message);
-                handle
-                .emit_and_trigger(
-                  "midi_message",
-                  MidiMessage {
-                    message: message.to_vec(),
-                  },
-                )
-                .map_err(|e| {
-                  println!("Error sending midi message: {}", e);
-                })
-                .ok();
-            },
-            (),
-          );
-          match midi_in_conn {
-            Ok(midi_in_conn) => {
-              midi_state.input.lock().unwrap().replace(midi_in_conn);
+fn open_midi_connection(midi_state: tauri::State<'_, MidiState>, window: Window<Wry>) {
+    let handle = Arc::new(window).clone();
+    let midi_in = MidiInput::new("ui_synth");
+    match midi_in {
+        Ok(midi_in) => {
+            let midi_in_ports = midi_in.ports();
+            let port = midi_in_ports.get(0);
+            match port {
+                Some(port) => {
+                    // Print the name of the port
+                    println!("Port: {}", midi_in.port_name(port).unwrap());
+                    let midi_in_conn = midi_in.connect(
+                        port,
+                        "midir",
+                        move |_, message, _| {
+                            println!("Message: {:?}", message);
+                            handle
+                                .emit_and_trigger(
+                                    "midi_message",
+                                    MidiMessage {
+                                        message: message.to_vec(),
+                                    },
+                                )
+                                .map_err(|e| {
+                                    println!("Error sending midi message: {}", e);
+                                })
+                                .ok();
+                        },
+                        (),
+                    );
+                    match midi_in_conn {
+                        Ok(midi_in_conn) => {
+                            midi_state.input.lock().unwrap().replace(midi_in_conn);
+                        }
+                        Err(e) => {
+                            println!("Error: {}", e);
+                        }
+                    }
+                }
+                None => {
+                    println!("No port found at index {}", 0);
+                }
             }
-            Err(e) => {
-              println!("Error: {}", e);
-            }
-          }
         }
-        None => {
-          println!("No port found at index {}", 0);
-        }
-      }
+        Err(e) => println!("Error: {}", e),
     }
-    Err(e) => println!("Error: {}", e),
-  }
 }
 
 #[tauri::command(async)]
-fn update_synth(
-    synth_state: tauri::State<'_, SynthState>,
-) {
+fn update_synth(synth_state: tauri::State<'_, SynthState>) {
     loop {
         let synth_state = &synth_state.synth;
         let mut synth = synth_state.lock().unwrap();
@@ -101,7 +95,7 @@ fn main() {
     // Get an output stream handle to the default physical sound device
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-    let synth =  Arc::new(Mutex::new(Synth::new(stream_handle)));
+    let synth = Arc::new(Mutex::new(Synth::new(stream_handle)));
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![open_midi_connection, update_synth])
@@ -115,18 +109,21 @@ fn main() {
                 let mut synth = synth_state.lock().unwrap();
 
                 // Deserialize the payload
-                let message = serde_json::from_str::<MidiMessage>(event.payload().unwrap()).unwrap();
+                let message =
+                    serde_json::from_str::<MidiMessage>(event.payload().unwrap()).unwrap();
                 let message = message.message;
 
                 let hz = 440.0 * 2.0_f32.powf((message[1] as f32 - 69.0) / 12.0);
                 let pressure = message[2] as f32 / 127.0;
 
-                if message[0] == 144 { // 144 is the event for note on
+                if message[0] == 144 {
+                    // 144 is the event for note on
                     let audio_source = Oscillator::sawtooth_wave(hz).amplify(pressure);
                     let envelope = Envelope::new(0.1, 0.2, 0.7, 1.3); // example envelope
                     synth.play_source(Box::new(audio_source), message[1], envelope)
                 }
-                if message[0] == 128 { // 128 is the event for note off
+                if message[0] == 128 {
+                    // 128 is the event for note off
                     synth.release_source(message[1])
                 }
             });
--- src-tauri/src/synth.rs
+++ src-tauri/src/synth.rs
@@ -1,5 +1,5 @@
-use std::time::Instant;
 use std::collections::HashMap;
+use std::time::Instant;
 
 use rodio::source::Source;
 use rodio::Sink;
@@ -9,117 +9,117 @@ struct EnvelopeState {
     envelope: Envelope,
     start_time: Instant,
     is_releasing: bool,
-	time_released: Option<Instant>,
+    time_released: Option<Instant>,
 }
 
 // The envelope struct
 pub struct Envelope {
-	attack: f32,
-	decay: f32,
-	sustain: f32,
-	release: f32,
+    attack: f32,
+    decay: f32,
+    sustain: f32,
+    release: f32,
 }
 
 impl Envelope {
-	pub fn new(attack: f32, decay: f32, sustain: f32, release: f32) -> Envelope {
-		Envelope {
-			attack,
-			decay,
-			sustain,
-			release,
-		}
-	}
+    pub fn new(attack: f32, decay: f32, sustain: f32, release: f32) -> Envelope {
+        Envelope {
+            attack,
+            decay,
+            sustain,
+            release,
+        }
+    }
 }
 
 pub struct Synth {
-	audio_sinks: HashMap<u8, Sink>,
-	envelope_states: HashMap<u8, EnvelopeState>,
-	stream_handle: rodio::OutputStreamHandle,
+    audio_sinks: HashMap<u8, Sink>,
+    envelope_states: HashMap<u8, EnvelopeState>,
+    stream_handle: rodio::OutputStreamHandle,
 }
 
 impl Synth {
-	pub fn new(stream_handle: rodio::OutputStreamHandle) -> Synth {
-		Synth {
-			audio_sinks: HashMap::new(),
-			envelope_states: HashMap::new(),
-			stream_handle,
-		}
-	}
+    pub fn new(stream_handle: rodio::OutputStreamHandle) -> Synth {
+        Synth {
+            audio_sinks: HashMap::new(),
+            envelope_states: HashMap::new(),
+            stream_handle,
+        }
+    }
 
-	pub fn play_source(&mut self, audio_source: Box<dyn Source<Item = f32> + Send>, source_id: u8, envelope: Envelope) {
-		let sink = Sink::try_new(&self.stream_handle).expect("Failed to create sink");
-		sink.append(audio_source);
+    pub fn play_source(
+        &mut self,
+        audio_source: Box<dyn Source<Item = f32> + Send>,
+        source_id: u8,
+        envelope: Envelope,
+    ) {
+        let sink = Sink::try_new(&self.stream_handle).expect("Failed to create sink");
+        sink.append(audio_source);
 
-		// let envelope = Envelope::new(0.1, 0.2, 0.7, 1.3); // example envelope
-		let envelope_state = EnvelopeState {
-			envelope,
-			start_time: Instant::now(),
-			is_releasing: false,
-			time_released: None,
-		};
+        // let envelope = Envelope::new(0.1, 0.2, 0.7, 1.3); // example envelope
+        let envelope_state = EnvelopeState {
+            envelope,
+            start_time: Instant::now(),
+            is_releasing: false,
+            time_released: None,
+        };
 
-		self.audio_sinks.insert(source_id, sink);
-		self.envelope_states.insert(source_id, envelope_state);
-	}
+        self.audio_sinks.insert(source_id, sink);
+        self.envelope_states.insert(source_id, envelope_state);
+    }
 
-	pub fn release_source(&mut self, source_id: u8) {
-		if let Some(envelope_state) = self.envelope_states.get_mut(&source_id) {
-			envelope_state.is_releasing = true;
-			envelope_state.time_released = Some(Instant::now());
-		}
-	}
+    pub fn release_source(&mut self, source_id: u8) {
+        if let Some(envelope_state) = self.envelope_states.get_mut(&source_id) {
+            envelope_state.is_releasing = true;
+            envelope_state.time_released = Some(Instant::now());
+        }
+    }
 
-	pub fn update(&mut self) {
-		let now = Instant::now();
+    pub fn update(&mut self) {
+        let now = Instant::now();
 
-		let mut to_remove = Vec::new();
+        let mut to_remove = Vec::new();
 
-		for (source_id, envelope_state) in self.envelope_states.iter_mut() {
-			let elapsed = now.duration_since(envelope_state.start_time).as_secs_f32();
+        for (source_id, envelope_state) in self.envelope_states.iter_mut() {
+            let elapsed = now.duration_since(envelope_state.start_time).as_secs_f32();
 
-			let envelope = &envelope_state.envelope;
-			let sink = self.audio_sinks.get_mut(source_id).unwrap();
+            let envelope = &envelope_state.envelope;
+            let sink = self.audio_sinks.get_mut(source_id).unwrap();
 
-			let volume = if elapsed < envelope.attack {
-				// Attack
-				// linear
-				elapsed / envelope.attack
-				// exponential
-				// (elapsed / envelope.attack).powf(2.0)
-			} else if elapsed < envelope.attack + envelope.decay {
-				// Decay
-				// linear
-				1.0 - (elapsed - envelope.attack) / envelope.decay * (1.0 - envelope.sustain)
-				// exponential
-				// 1.0 - (elapsed - envelope.attack) / envelope.decay * (1.0 - envelope.sustain).powf(2.0)
-			} else if envelope_state.is_releasing {
-				// Release
-				let elapsed_since_released = now.duration_since(envelope_state.time_released.unwrap()).as_secs_f32().min(envelope.release);
-				// linear
-				envelope.sustain - elapsed_since_released / envelope.release * envelope.sustain
-				// exponential
-				// envelope.sustain * (1.0 - elapsed_since_released / envelope.release).powf(2.0)
-			} else {
-				// Sustain
-				envelope.sustain
-			};
+            let volume = if elapsed < envelope.attack {
+                // Attack
+                elapsed / envelope.attack
+            } else if elapsed < envelope.attack + envelope.decay {
+                // Decay
+                1.0 - (elapsed - envelope.attack) / envelope.decay * (1.0 - envelope.sustain)
+            } else if envelope_state.is_releasing {
+                // Release
+                let elapsed_since_released = now
+                    .duration_since(envelope_state.time_released.unwrap())
+                    .as_secs_f32()
+                    .min(envelope.release);
+                envelope.sustain - elapsed_since_released / envelope.release * envelope.sustain
+            } else {
+                // Sustain
+                envelope.sustain
+            };
 
-			sink.set_volume(volume);
+            sink.set_volume(volume);
 
-			
-			if envelope_state.is_releasing {
-				let elapsed_since_released = now.duration_since(envelope_state.time_released.unwrap()).as_secs_f32();
-				if elapsed_since_released >= envelope.release + 0.1 {
-					// This is done as a seperate step to avoid a second mutable borrow of self.envelope_states
-					// First borrow is when .iter_mut() is called, second is when .remove() is called
-					to_remove.push(*source_id);
-				}
-			}
-		}
+            if envelope_state.is_releasing {
+                let elapsed_since_released = now
+                    .duration_since(envelope_state.time_released.unwrap())
+                    .as_secs_f32();
+                if elapsed_since_released >= envelope.release + 0.1 {
+                    // This is done as a seperate step to avoid a second mutable borrow of self.envelope_states
+                    // First borrow is when .iter_mut() is called, second is when .remove() is called
+                    to_remove.push(*source_id);
+                }
+            }
+        }
 
-		for source_id in to_remove {
-			self.envelope_states.remove(&source_id);
-			self.audio_sinks.remove(&source_id);
-		}
-	}
+        for source_id in to_remove {
+            self.envelope_states.remove(&source_id);
+            self.audio_sinks.remove(&source_id);
+        }
+    }
 }
